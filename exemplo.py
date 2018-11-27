6135db3eae08d21c5bacd0c631e277989400de9b
Aluno <diegogon420@hotmail.com>
2018-11-27 15:47:39 -0200

    implementei o KNN

--- exemplo.py
+++ exemplo.py
@@ -2,15 +2,12 @@ import cv2, os, re
 from skimage.feature import local_binary_pattern
 from scipy.stats import itemfreq
 '''0 import classificadores'''
-from sklearn import svm, tree
-from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn import tree
 
-diretorio= "/Users/Marcos/Documents/GitHub/TCC/TCC/database"
+
+diretorio= "./database/"
 
 """1 funçao natural_sort"""
 def natural_sort(l):
@@ -23,52 +20,62 @@ content = os.listdir(diretorio)
 content = natural_sort(content)
 
 '''3 vetores treino e teste'''
-treinoimagem = []
-treinorotulo = []
-testeimagem = []
-testerotulo = []
+treinoImagem = []
+treinoRotulo = []
+testeImagem = []
+testeRotulo = []
 
 '''4 Para cada pessoa nos diretorios, imprima a pasta delas'''
 for pessoa in content:
-        diretorioPessoas = diretorio + pessoa + '/'
-        if not os.path.isdir(diretorioPessoas): continue
+    diretorioPessoas = diretorio + pessoa + '/'
+    if not os.path.isdir(diretorioPessoas): continue
 
-        print(diretorioPessoas)
+    #print(diretorioPessoas)
 
-        contentPessoas = os.listdir(diretorioPessoas)
-        contentPessoas = natural_sort(contentPessoas)
+    contentPessoas = os.listdir(diretorioPessoas)
+    contentPessoas = natural_sort(contentPessoas)
 
-'''6 contador pra condição da metade para o treino e metade pro teste'''
-contador = 0;
+    '''6 contador pra condição da metade para o treino e metade pro teste'''
+    contador = 1;
 
-'''7 Para o contador (indicador) na pasta das pessoas, faça:'''
-for cont in contentPessoas:
-    nomeImg = diretorioPessoas + cont
-    #converte a imagem para cinza
-    imagemCinza = cv2.cvtColor(ler_imagem, cv2.COLOR_RGB2GRAY)
+    '''7 Para o contador (indicador) na pasta das pessoas, faça:'''
+    for img in contentPessoas:
+        nomeImg = diretorioPessoas + img
+        #converte a imagem para cinza
+        imagemCinza = cv2.cvtColor(cv2.imread(nomeImg), cv2.COLOR_RGB2GRAY)
 
-    pontos = 8
-    raio = 2
-    metodo = "nri_uniform"
+        pontos = 8
+        raio = 2
+        metodo = "nri_uniform"
 
-    lbp = local_binary_pattern(imagemCinza, pontos, raio, method = metodo )
-    print(lbp)
+        lbp = local_binary_pattern(imagemCinza, pontos, raio, method = metodo )
+        # print(lbp)
 
-    x = itemfreq(lbp)
-    somaTotal = sum(x[:, 1])
+        x = itemfreq(lbp)
+        somaTotal = sum(x[:, 1])
 
-'''8 se o contador (lá em cima no passo 6) for menor que dois, adicione 2
-imagens para o treinamento img (passo 3)'''
-if contador < 2:
-    treinoImagem.append(x[:, 1]/somaTotal)
-    treinoRotulo.append(pessoa)
-    contador = contador + 1
-else:
-    if contador < 4:
-        testeImagem.append(x[:, 1]/somaTotal)
-        testeRotulo.append(pessoa)
+        '''8 se o contador (lá em cima no passo 6) for menor que dois, adicione 2
+        imagens para o treinamento img (passo 3)'''
+        if contador <=2: #quantidade de amostras no treino
+            treinoImagem.append(x[:, 1]/somaTotal)
+            treinoRotulo.append(pessoa)
+        else:
+            #o restante é do teste
+            testeImagem.append(x[:, 1]/somaTotal)
+            testeRotulo.append(pessoa)
+
         contador = contador + 1
 
 '''objetos dos classificadores'''
-classKNN = svm.SVC(kernel='rbf', probability=True)
-classSVM = KNeighborsClassifier(n_neighbors = 1)
+# classKNN = svm.SVC(kernel='rbf', probability=True)
+
+classKNN = KNeighborsClassifier(n_neighbors = 1)
+#manda o classificador aprender
+'''treinamento'''
+classKNN.fit(treinoImagem, treinoRotulo)
+'''classificacao'''
+predicao = classKNN.predict(testeImagem)
+print(predicao)
+
+acuracia = classKNN.score(testeImagem, testeRotulo)
+print(acuracia*100)
